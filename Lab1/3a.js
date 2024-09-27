cf74343f7c7df932bf24990006e94067ebe2f807
Darragh <darraghrohan@gmail.con>
2024-09-27 09:41:55 +0000

    3a.js finished

--- Lab1/3a.js
+++ Lab1/3a.js
@@ -1,2 +1,47 @@
-//Array of strings
-let tasks = [];
+// 3a. Array of strings
+let Tasks = ["Walking", "Hiking"];
+
+//3b. Create an addTask function
+//(i)Recieves a string as a param
+let addTask = (task) => {
+    //(ii)Add task to tarray
+    Tasks.push(task);
+    //(iii)Print out message
+    console.log(task + " has been added to the array");
+    //(iv)Find number of elements in the array
+    console.log(Tasks.length);
+}
+
+addTask("Work");
+
+//3c. Create a listAllTasks function
+let listAllTasks = () => {
+    //(i) Iterates over all the tasks in the array
+    Tasks.forEach((element) => {
+        //(ii)Print out
+        console.log(element);
+    })
+}
+
+addTask("Cooking");
+listAllTasks();
+
+//3d. Delete task function
+//(i)Recieve a string as a parameter
+let deleteTask = (task) => {
+    let index = Tasks.indexOf(task);
+
+    //(ii)Remove the string from the array
+    if (index > -1) {
+        Tasks.splice(index, 1);
+        //(iii) Prints a message indicating deletion
+        console.log(task + " has been removed from the array");
+    }
+    else {
+        console.log(task + " has not been found in the array");
+    }
+}
+
+//Call deleteTask function
+addTask("Work");
+deleteTask("Cooking");
