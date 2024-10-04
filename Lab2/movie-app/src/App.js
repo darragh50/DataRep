f833cd2e7970606ba918a3f13a0766b7c4ba03dc
Darragh <darraghrohan@gmail.con>
2024-10-04 09:19:40 +0000

    Ex5 Complete - Navigation & React routing

--- Lab2/movie-app/src/App.js
+++ Lab2/movie-app/src/App.js
@@ -4,19 +4,24 @@ import './App.css';
 import Header from './components/header';
 import Content from './components/content';
 import Footer from './components/footer';
+//Ex4. Import Bootstrap's CSS 
+import 'bootstrap/dist/css/bootstrap.min.css';
+//Ex5. Import react router
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavigationBar from './components/navbar';
 
 function App() {
   return (
-    <div>
-      {/**Ex2. Print "Hello Wordl" & the current time
-      <he>Hello World!</he>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>*/}
-
-      {/**Ex3. Call created components */}
-      <Header/>
-      <Content/>
-      <Footer/>
-    </div>
+    //Ex5. Set up routing - create each link
+    <Router>
+    <NavigationBar />
+    <Routes>
+      <Route path="/home" element={<Content />} />
+      <Route path="/read" element={<h1>Read Component</h1>} />
+      <Route path="/create" element={<h1>Create Component</h1>} />
+    </Routes>
+    <Footer />
+  </Router>
   );
 }
 
---
+++ Lab2/movie-app/src/components/navbar.js
@@ -0,0 +1,24 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+const NavigationBar = () => {
+  return (
+    //Create a responsive navigation bar
+    //Use React Router's Link to handle navigation
+    //between different pages
+        <Navbar bg="dark" data-bs-theme="dark">
+          <Container>
+            <Navbar.Brand href="/">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/create">Create</Nav.Link>
+              <Nav.Link href="/read">Read</Nav.Link>
+            </Nav>
+          </Container>
+      </Navbar>
+  );
+};
+
+export default NavigationBar;
