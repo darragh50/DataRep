594a8e8a7dc80237d9b3708661363cb7d1c46ac1
Darragh <darraghrohan@gmail.con>
2024-10-04 08:36:32 +0000

    Create Lab2 & complete exercise 2

---
+++ Lab2/movie-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      {/**Ex2. Print "Hello Wordl" & the current time */}
+      <he>Hello World!</he>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+}
+
+export default App;
