6993de83dabb6aab8b311a52fd07465ecb7d30c1
Darragh <darraghrohan@gmail.con>
2024-10-18 09:05:10 +0000

    Ex3a & b. Form to upload data  & handleSubmit()

--- Lab2/movie-app/src/components/create.js
+++ Lab2/movie-app/src/components/create.js
@@ -1,6 +1,32 @@
-//Lab 3. Task 1. Create new function components
-const Create = () => {
-    return <h3>Hello from the Create component</h3>;
-  };
-  
-  export default Create;
+import { useState } from "react";
+
+function Create() {
+  const [title, setTitle] = useState('');
+
+  //Handle new events from the button click and log information
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title);
+  }
+
+  //Return all values referenced in handleSubmit()
+  return (
+    <div>
+      <h2>This is my Create Component.</h2>
+      {/**Form that will upload data to a server */}
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Add Movie Title: </label>
+          <input type="text"
+            className="form-control"
+            value={title}
+            onChange={(e) => { setTitle(e.target.value) }}
+          />
+        </div>
+        <input type="submit" value="Add Movie" />
+      </form>
+    </div>
+  );
+}
+
+export default Create;
