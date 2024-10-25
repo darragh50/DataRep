33c155f24976b6b966df619f19c79f6f6398600b
Darragh <darraghrohan@gmail.con>
2024-10-25 08:45:32 +0000

    Ex4. Add route with URL param & add second route

--- Lab5/server.js
+++ Lab5/server.js
@@ -7,6 +7,20 @@ app.get('/', (req, res) => {
     res.send('Welcome to Data Respresentation & Querying');
 });
 
+//Add a route with URL parameter
+app.get('/hello/:name', (req, res) => {
+    const name = req.params.name;
+    res.send(`Hello ${name}`);
+});
+
+//Add second URL paramater for surname
+app.get('/hello/:name/:surname', (req, res) => {
+    const name = req.params.name;
+    const surname = req.params.surname;
+    res.send(`Hello ${name}  ${surname}`);
+});
+
+
 //Server listening on port ${port}
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
